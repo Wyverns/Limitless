f2649b383f889b68761041fd1fd0516de3ab0f48
TheUltiOne <75040208+TheUltiOne@users.noreply.github.com>
2021-01-04 23:11:42 -0500

    Documentation updated

--- cogs/pre/examplecog.py
+++ cogs/pre/examplecog.py
@@ -1,11 +1,12 @@
 from discord.ext import commands
-from cogs.pre.utils import checks
+from cogs.pre.utils import checks # You can make your own utils, or use the pre-made utils we have.
 
 
 class examplecog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+    
+    # An example command called examplecog. It's really simple. When run, a message will be sent with content of 'Hello! I am an example cog.'
     @checks.is_cog_contributor()
     @commands.command(name='examplecog')
     async def _example(self, ctx):
